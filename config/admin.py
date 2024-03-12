55f19ec082dc7f7fc54211c9e1ef673b74698156
Dominic Chingoma <dominic.chingoma@trustgro.com>
2024-03-12 11:30:17 +0300

    adding agents

--- config/admin.py
+++ config/admin.py
@@ -5,27 +5,44 @@ from .models import *
 # class PolicyNameAdmin(admin.ModelAdmin):
 #     list_display = ['name', "policy_type"]
 
+
 @admin.register(InsuranceCompany)
 class InsuranceCompanyAdmin(admin.ModelAdmin):
-    list_display = ['name',]
+    list_display = [
+        "name",
+    ]
+
 
 @admin.register(DocumentType)
 class DocumentTypeAdmin(admin.ModelAdmin):
-    list_display = ['document_type', 'category']
+    list_display = ["document_type", "category"]
+
 
 @admin.register(Relationships)
 class RelationshipsAdmin(admin.ModelAdmin):
-    list_display = ['name',]
+    list_display = [
+        "name",
+    ]
+
 
 @admin.register(IdDocumentType)
 class IdDocumentTypeAdmin(admin.ModelAdmin):
-    list_display = ['type_name',]
+    list_display = [
+        "type_name",
+    ]
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ["agent_name", "entity_type", "phone_number", "email"]
+    search_fields = ["agent_name", "entity_type", "phone_number", "email"]
 
 
 # definition of additional claimfields
 class ClaimFieldsInline(admin.TabularInline):
     model = ClaimFields
     extra = 1
+
 
 class ClaimAdmin(admin.ModelAdmin):
     inlines = (ClaimFieldsInline,)
