5d38bcdc42c3216dc6c383fdcb32ad59fa22f869
tarbabyelvis <tarbaby.banda@fin.africa>
2024-10-11 10:22:20 +0300

    get scores

--- integrations/back_office.py
+++ integrations/back_office.py
@@ -5,6 +5,7 @@ from FinCover.settings import BACK_OFFICE_URL
 
 def __make_backoffice_request(tenant_id, uri, payload):
     back_office_url = generate_back_office_url(tenant_id, uri)
+    print(f'endpoint we hitting {back_office_url}')
     return requests.post(
         url=back_office_url,
         json=payload,
